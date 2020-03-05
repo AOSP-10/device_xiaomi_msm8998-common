cd0b8a8bb6f32b7d7011be50e07d6b2ece1265cd
cooluc <sbwml@qq.com>
2020-03-05 18:09:48 +0800

    msm8998-common: import Chinese translation

--- msm8998.mk
+++ msm8998.mk
@@ -21,7 +21,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-language
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
